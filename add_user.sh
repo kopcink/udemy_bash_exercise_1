7945c24e76382752b3827084374c4c3b7ad84ef5
Karol Kopcinski <karolkopcinski@gmail.com>
2023-02-06 11:05:06 +0100

    Add creating a user functionality

--- add_user.sh
+++ add_user.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-if [[ "${UID}" -ne 0 ]]
+if [[ "$UID" -ne 0 ]]
     then echo "You need to execute the script with root privileges."
+    exit 1
+fi
+
+read -p "Provide a username: " USERNAME
+read -p "Provide an initial password: " PASSWORD
+
+useradd -ms /bin/bash "${USERNAME}"
+
+if [[ "$?" -ne 0 ]]
+then
+    echo "The script was not able to create a user ${USERNAME}."
     exit 1
 fi
 
